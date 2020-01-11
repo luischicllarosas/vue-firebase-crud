b509e387f512ed88b3f5da1b2b7b64be6c0b6132
Luis Chiclla <luischicllarosas@gmail.com>
2020-01-10 19:45:42 -0500

    Cambie el titulo principal

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
     <div class="hero is-primary">
         <div class="hero-body">
-            <h1 class="is-size-1 has-text-centered">Bienvenido al demo de VueJS y Firebase</h1>
+            <h1 class="is-size-1 has-text-centered">Bienvenido al demo de VueJS y Firebase con bulma</h1>
         </div>
     </div>
 </template>
-
 <script>
     export default {
         
     }
 </script>
-
 <style scoped>
-
 </style>
